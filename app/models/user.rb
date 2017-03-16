9a1f443a7ca65fb0d3be8fc1f3323f53b00f0f48
dcheley <danielcheley@hotmail.com>
2017-03-16 13:08:50 -0400

    Changing validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
