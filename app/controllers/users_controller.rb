e38b8c287b53d823c7e4f363c864186d4d98996b
dcheley <danielcheley@hotmail.com>
2017-03-16 11:57:44 -0400

    Adding new & create logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    if @user.save
+      redirect_to "/msg", notice: "#{@user.email} account created!"
+    else
+      render :new, alert: "Error occurred during sign up, please try again."
+    end
   end
 
   private
