4bdacef8f1037284db9b0e9d799a2da37a397093
dcheley <danielcheley@hotmail.com>
2017-03-17 15:07:59 -0400

    Adding validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :email, presence: true
   validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => :create }
   validates :email, uniqueness: true
 end
