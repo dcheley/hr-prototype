9783b5363a9a6fb9c3060f890a776e005af53f26
dcheley <danielcheley@hotmail.com>
2017-03-20 16:03:34 -0400

    Requiring load_user before update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :load_user, only: [:show, :edit]
+  before_action :load_user, only: [:show, :edit, :update]
 
   def new
     @user = User.new
