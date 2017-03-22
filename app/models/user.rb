da5d71aeabd42965ca8fb5001051a42420c995b6
dcheley <danielcheley@hotmail.com>
2017-03-22 10:48:02 -0400

    Commenting password related code for smoke test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_secure_password
+  # has_secure_password
 
-  validates :password, length: { minimum: 6 }
+  # validates :password, length: { minimum: 6 }
   validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => :create }
   validates :email, uniqueness: true
 end
