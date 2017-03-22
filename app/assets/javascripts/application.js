0c02578b2d4399c9ad39691e3deec7d296190a45
dcheley <danielcheley@hotmail.com>
2017-03-22 09:34:19 -0400

    Adding feedback to users index, styling org chart and adding jquery animations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,4 +29,56 @@ $(document).ready(function() {
   var $levelParentClass = '.level';
   var $listParentClass = '.list';
   var $activeParentClass = '.active';
+
+  // move items
+  function moveSelected($itemSelected) {
+    var $levelParent = $($itemSelected).closest($levelParentClass);
+    var $listParent = $($itemSelected).closest($listParentClass);
+    var $itemContainer = $($itemSelected).parent();
+    var $destination = $($listParent).siblings($activeParentClass).children().first();
+    var $newContent = $($itemSelected).html();
+
+    // update selected content section
+    $($destination).html($newContent);
+
+    // fade animation to look like refresh
+    $($listParent).siblings($activeParentClass).fadeOut(0).fadeIn(300);
+    $($levelParent).nextAll().not('.hidden').fadeOut(0).fadeIn(300);
+
+    // toggle active item
+    $($itemContainer).children().removeClass('selection');
+    $($itemSelected).addClass('selection');
+
+    // show next row (should load new items)
+    $($levelParent).next().removeClass('hidden');
+    $($levelParent).addClass('expanded');
+  }
+
+  // smooth scroll to current level
+  function scrollParent($scrollTarget) {
+    var target = $scrollTarget;
+    event.preventDefault();
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top-15
+        }, 700);
+        return false;
+      }
+  }
+
+  // listeners
+  $(".org-header").on("click", function(){
+      scrollParent($(this).closest('.level'));
+      moveSelected($(this).closest('.element'));
+  });
+
+  $(document).on('mouseenter','.org-header', function (event) {
+     $(this).closest('.element').addClass('hover');
+     $(this).closest('.level').addClass('hover-expanded');
+     $(this).closest('.level').next('.level').addClass('hover-children');
+  }).on('mouseleave','.info-header',  function(){
+     $(this).closest('.element-item').removeClass('hover');
+     $(this).closest('.level').removeClass('hover-expanded');
+     $(this).closest('.level').next('.level').removeClass('hover-children');
+  });
 });
