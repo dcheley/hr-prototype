f2091f09806f87a77ef528efc3dde6c95d0e2e44
dcheley <danielcheley@hotmail.com>
2017-03-23 13:56:20 -0400

    Changing successful login redirect route and uncommenting password validation and bcrypt helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to edit_user_url(user), notice: "Logged in!"
+      redirect_to "/pages/org_charts", notice: "Logged in!"
     else
       flash[:alert] = "Invalid email or password"
       render :new
