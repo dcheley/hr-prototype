d17245ee90879e912775845b11e87fb5e7772469
dcheley <danielcheley@hotmail.com>
2017-03-23 15:10:01 -0400

    Creating ADMO seeds for development env

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create([{ name: "Rob B.", email: "robb@email.com", phone: "(416) 327-3682", title: "Assistant Deputy Minister Chief Administrative Officer", adm_office: true, adm: true },
+  { name: 'Judi P.', email: "jp@email.com", phone: "(416) 326-7039", title: "Executive Assistant", adm_office: true, staff: true },
+  { name: "Conn Car", email: "cc@email.com", phone: "(416) 325-6866", title: "Admin. Assistant", adm_office: true, staff: true },
+  { name: "Nat R.", email: "natr@email.com", phone: "(416) 212-4215", title: "Events & Communications Coordinator", adm_office: true, staff: true },
+  { name: "Nad Alj", email: "nad.alj@emailcom", phone: "(416) 906-5018", adm_office: true, staff: true },
+  { name: "Cat Gui", email: "cg@email.com", phone: "(416) 325-6828", adm_office: true, staff: true },
+  { name: "Iso Kuzm", email: "isku@email.com", phone: "(416) 325-9366", adm_office: true, staff: true }])
