3349f0845dd6fbee5a264476de70c890923743c2
dcheley <danielcheley@hotmail.com>
2017-03-23 14:04:02 -0400

    Resetting db

--- db/schema.rb
+++ db/schema.rb
@@ -15,23 +15,6 @@ ActiveRecord::Schema.define(version: 20170322193032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bars", force: :cascade do |t|
-    t.string   "name"
-    t.string   "address"
-    t.string   "phone"
-    t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "teams", force: :cascade do |t|
-    t.string   "name"
-    t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "division"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.datetime "created_at",                        null: false
