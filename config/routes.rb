739cf4a02ccc9e74c271c09a86f192ad209fb833
dcheley <danielcheley@hotmail.com>
2017-03-27 14:06:37 -0400

    Changing root url back to previous syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "pages#show", page: "home"
+  root "pages#home"
+
+  get '/pages/*page' => 'pages#show'
 
   get '/users/admo_org_chart' => 'users#admo_org_chart'
   get '/users/bpf_org_chart' => 'users#bpf_org_chart'
@@ -8,8 +10,6 @@ Rails.application.routes.draw do
   get '/users/smf_org_chart' => 'users#smf_org_chart'
 
   resources :users, only: [:new, :create, :show, :edit, :update, :index]
-
-  get '/pages/*page' => 'pages#show'
 
   get 'login' => 'sessions#new', as: :login
   post 'login' => 'sessions#create'
