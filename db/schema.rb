631b3cfb7a6570d25c6072629d36e9b6f67daff4
dcheley <danielcheley@hotmail.com>
2017-03-28 09:42:10 -0400

    Adding paperclip columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322193032) do
+ActiveRecord::Schema.define(version: 20170328134107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20170322193032) do
     t.boolean  "business_planning_and_finance"
     t.string   "password_digest"
     t.boolean  "staff"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
