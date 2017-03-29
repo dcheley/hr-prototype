e6c26754f6ef7bee408c4e1bd347ee22a1ae90fb
dcheley <danielcheley@hotmail.com>
2017-03-29 15:34:43 -0400

    Changing login redirect route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to "/pages/org_charts", notice: "Logged in!"
+      redirect_to "/users/org_charts", notice: "Logged in!"
     else
       flash[:alert] = "Invalid email or password"
       render :new
