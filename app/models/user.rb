c1a065b48e3b9c9c78aa4563e9fa286a174eff88
dcheley <danielcheley@hotmail.com>
2017-03-30 10:02:07 -0400

    Creating one to many relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
+  has_many :badges
+
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   validates :password, length: { minimum: 6, :on => :create }
   validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :on => :create }
