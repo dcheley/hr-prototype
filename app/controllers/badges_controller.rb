d095f5d922363b7bcdf4e3e55ea7d952dbffd9f2
dcheley <danielcheley@hotmail.com>
2017-03-30 10:08:52 -0400

    Setting strong params and creating before action methods for later use

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -13,4 +13,18 @@ class BadgesController < ApplicationController
 
   def destroy
   end
+
+private
+
+  def load_user
+    @user = User.find(params[:user_id])
+  end
+
+  def load_badge
+    @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+    params.require(:badge).permit(:user_id, :name, :red, :blue, :yellow, :green, :grey, :purple, :pink)
+  end
 end
