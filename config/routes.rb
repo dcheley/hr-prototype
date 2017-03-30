1fc42626f3eb91bd492100825b70c601bbbe38a1
dcheley <danielcheley@hotmail.com>
2017-03-30 10:05:29 -0400

    Generating badges controller and adding new nested route for badges

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'badges/new'
+
+  get 'badges/create'
+
+  get 'badges/update'
+
+  get 'badges/edit'
+
+  get 'badges/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#home"
 
@@ -13,7 +23,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show, :edit, :update, :index]
 
   resources :users do
-    resources :badges, only: [:create, :update, :edit, :destroy]
+    resources :badges, only: [:new, :create, :update, :edit, :destroy]
   end
 
   get 'login' => 'sessions#new', as: :login
