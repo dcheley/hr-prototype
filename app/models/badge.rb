d37d2ae3d6fe3a91f9ada085ecf344e8685ce0d2
dcheley <danielcheley@hotmail.com>
2017-03-31 10:02:53 -0400

    Creating model method and implementing in before_save callback

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,4 +2,10 @@ class Badge < ApplicationRecord
   belongs_to :users, optional: true
 
   validates :name, presence: true
+
+  before_save :falsify_others
+
+  def falsify_others
+    Badge.where('id != ? and default', self.id).update_all("default = 'false'")
+  end
 end
