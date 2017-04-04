241c0beeb4d41ac1c1aad101f60a5f0791230fa0
dcheley <danielcheley@hotmail.com>
2017-04-04 16:25:59 -0400

    Adding nested opportunities routes and removing whitespace on view

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,12 @@ Rails.application.routes.draw do
   get '/users/bpf_org_chart' => 'users#bpf_org_chart'
   get '/users/hr_org_chart' => 'users#hr_org_chart'
   get '/users/smf_org_chart' => 'users#smf_org_chart'
-  
+
   resources :users, only: [:new, :create, :show, :update, :index] #Edit route blocked for now
 
   resources :users do
     resources :badges, only: [:new, :create, :show, :update, :edit, :destroy]
+    resources :opportunities, only: [:index, :show]
   end
 
   get 'login' => 'sessions#new', as: :login
