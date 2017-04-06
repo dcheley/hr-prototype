d060bf419ca366713219f69d3fccdc8d3345f9c7
dcheley <danielcheley@hotmail.com>
2017-04-06 15:48:41 -0400

    Adding custom validation to require a badge colour

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,6 +1,13 @@
 class Badge < ApplicationRecord
   belongs_to :users, optional: true
   belongs_to :opportunities, optional: true
-  
+
   validates :name, presence: true
+  validate :at_least_one_colour
+
+  def at_least_one_colour
+    if [self.red, self.blue, self.purple, self.green, self.yellow, self.grey, self.pink].reject(&:blank?).size == 0
+      errors[:base] << ("Badge colour required.")
+    end
+  end
 end
