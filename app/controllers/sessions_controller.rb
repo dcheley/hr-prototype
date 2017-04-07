bb2dc425e20b3078540b4b8a7d3a1533fb611a92
dcheley <danielcheley@hotmail.com>
2017-04-07 12:49:06 -0400

    Commenting out homepage link and badge creation on sign up, also changing redirect route and notification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password]) && user.badges.ids == []
       session[:user_id] = user.id
-      Badge.create(name: "Signed up",
-        user_id: user.id,
-        blue: true)
-      redirect_to "/users/org_charts", notice: "Logged in! Thanks for signing up!"
+      #Badge.create(name: "Sign Up",
+      #   user_id: user.id,
+      #   blue: true)
+      redirect_to "/users/#{user.id}", notice: "To get started, scroll down and add badges that depict your current skills and experience."
     elsif user && user.authenticate(params[:password]) && user.badges.ids != []
       session[:user_id] = user.id
       redirect_to "/users/org_charts", notice: "Logged in!"
