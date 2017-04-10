2515e5e256546c268ec555d96aedea9ca918bfb7
dcheley <danielcheley@hotmail.com>
2017-04-10 11:04:53 -0400

    Creating badge helper methods to count endorsements

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,2 +1,31 @@
 module BadgesHelper
+  def display_likes(badge)
+    votes = badge.votes_for.up.by_type(User)
+    return list_likers(votes) if votes.size <= 8
+    count_likers(votes)
+  end
+
+private
+
+  def list_likers(votes)
+    names = []
+    unless votes.blank?
+      votes.voters.each do |v|
+        names.push(link_to voter.name,
+        user_path(voter.name),
+        class: 'name')
+      end
+      names.to_sentence.html_safe + like_plural(votes)
+    end
+  end
+
+  def count_likers(votes)
+    vote_count = votes.size
+    vote_count.to_s + ' endorsements'
+  end
+
+  def like_plural(votes)
+    return ' endorse badge' if votes.count > 1
+    ' endorses this'
+  end
 end
