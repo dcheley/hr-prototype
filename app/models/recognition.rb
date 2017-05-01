ce52b333be2e5548b6e84e0008f0403aa115538f
dcheley <danielcheley@hotmail.com>
2017-05-01 11:10:06 -0400

    Adding validations and acts as votable helper

--- app/models/recognition.rb
+++ app/models/recognition.rb
@@ -1,4 +1,9 @@
 class Recognition < ApplicationRecord
+  acts_as_votable
+
   belongs_to :receiver, class_name: :User
   belongs_to :recognizer, class_name: :User
+
+  validates :name, presence: true
+  validates :description, length: {maximum: 500}
 end
