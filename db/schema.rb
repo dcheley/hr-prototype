d3a5285931b20397f76ccfd56d8e37977700c964
dcheley <danielcheley@hotmail.com>
2017-05-09 16:13:51 -0400

    Adding career aspirations column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509195512) do
+ActiveRecord::Schema.define(version: 20170509201307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20170509195512) do
     t.datetime "avatar_updated_at"
     t.boolean  "co_op"
     t.string   "direct_report"
+    t.string   "career_aspirations"
   end
 
   create_table "votes", force: :cascade do |t|
