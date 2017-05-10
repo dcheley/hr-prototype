dcf9a4e965f8a6506f78e854aeb70f12b3602eea
dcheley <danielcheley@hotmail.com>
2017-05-10 16:27:49 -0400

    Adding agreement boolean column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509201307) do
+ActiveRecord::Schema.define(version: 20170510202717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20170509201307) do
     t.boolean  "co_op"
     t.string   "direct_report"
     t.string   "career_aspirations"
+    t.boolean  "agreement"
   end
 
   create_table "votes", force: :cascade do |t|
