7491d7ff97cd1f372f08b565c6a69380b4aaeb75
dcheley <danielcheley@hotmail.com>
2017-05-10 14:52:34 -0400

    Adding different login redirect for new co-op students

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,15 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password]) && user.badges.ids == []
+    if user && user.authenticate(params[:password]) && user.badges.ids == [] && user.co_op == nil
       session[:user_id] = user.id
       #Badge.create(name: "Sign Up",
       #   user_id: user.id,
       #   blue: true)
       redirect_to user_url(user.id), notice: %Q[To get started, click the ADD Badge link below to showcase your current skills and experience.]
+    elsif user && user.authenticate(params[:password]) && user.badges.ids == [] && user.co_op != nil
+      session[:user_id] = user.id
+      redirect_to "/pages/disclaimer", notice: "Welcome! Read the disclaimer below before creating your account"
     elsif user && user.authenticate(params[:password]) && user.badges.ids != []
       session[:user_id] = user.id
       redirect_to "/users/org_charts", notice: "Logged in!"
