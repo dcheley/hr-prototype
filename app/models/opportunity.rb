f36319412fa71c34a66cbcabc6f5cae98c608a37
dcheley <danielcheley@hotmail.com>
2017-05-11 11:36:38 -0400

    Adding paperclip photo helper

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -2,5 +2,7 @@ class Opportunity < ApplicationRecord
   has_many :badges
   has_many :users, through: :badges
 
+  has_attached_file :avatar, styles: { medium: "300x300>" }, default_url: "/images/:style/missing.png"
+
   validates :name, uniqueness: true
 end
