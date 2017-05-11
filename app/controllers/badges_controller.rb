d5bd467cc483680de3f2dfc2ec1e4f2299203802
dcheley <danielcheley@hotmail.com>
2017-05-11 14:54:45 -0400

    Calling new columns in view and adding to strong params

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -83,6 +83,8 @@ private
   end
 
   def badge_params
-    params.require(:badge).permit(:user_id, :opportunity_id, :receiver_id, :recognizer_id, :recognition_id, :name, :red, :blue, :yellow, :green, :grey, :purple, :pink, :description)
+    params.require(:badge).permit(:user_id, :opportunity_id, :receiver_id, :recognizer_id,
+    :recognition_id, :name, :red, :blue, :yellow, :green, :grey, :purple, :pink, :description,
+    :exp, :education)
   end
 end
