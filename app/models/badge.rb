c49611c25e0ceea2e4e7faa53fca6ed2315daad8
dcheley <danielcheley@hotmail.com>
2017-05-11 15:34:21 -0400

    Adding custom badge validation and checkboxes to badge form

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -8,11 +8,18 @@ class Badge < ApplicationRecord
 
   validates :name, presence: true
   validates :description, length: {maximum: 500}
+  validate :min_one
   # validate :at_least_one_colour
 
-  def at_least_one_colour
-    if [self.red, self.blue, self.purple, self.green, self.yellow, self.grey, self.pink].reject(&:blank?).size == 0
-      errors[:base] << ("Badge colour required.")
+  # def at_least_one_colour
+  #   if [self.red, self.blue, self.purple, self.green, self.yellow, self.grey, self.pink].reject(&:blank?).size == 0
+  #     errors[:base] << ("Badge colour required.")
+  #   end
+  # end
+
+  def min_one
+    if [self.exp, self.education].reject(&:blank?).size == 0
+      errors[:base] << ("Badge type required.")
     end
   end
 end
