2741b2dc4faf38bd61462245536ea902198794ed
dcheley <danielcheley@hotmail.com>
2017-05-12 09:17:30 -0400

    Switching variables

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -2,7 +2,7 @@ class OpportunitiesController < ApplicationController
   before_action :load_opportunity [:show, :like, :unlike]
 
   def show
-    @badge = current_user.badges.build
+    @opportunity = current_user.opportunities.build
   end
 
   def index
@@ -10,18 +10,18 @@ class OpportunitiesController < ApplicationController
   end
 
   def like
-    @badge.liked_by current_user
+    @opportunity.liked_by current_user
     respond_to do |format|
       format.html { redirect_to :back }
-      format.json { @badge }
+      format.json { @opportunity }
     end
   end
 
   def unlike
-    @badge.unliked_by current_user
+    @opportunity.unliked_by current_user
     respond_to do |format|
       format.html { redirect_to :back }
-      format.json { @badge }
+      format.json { @opportunity }
     end
   end
 
