454b8e19032c384523d62455bfd2f8c1a10718ce
dcheley <danielcheley@hotmail.com>
2017-05-18 11:53:14 -0400

    Seeding admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -143,7 +143,8 @@ User.create([{ name: "Robert B", email: "robb@email.com", phone: "(416) 222-2222
   { name: "Deni O", email: "deni@email.com", phone: "(416) 111-1111", title: "Co-op Student", co_op: true, staff: true, password: "790969" },
   { name: "Ian T", email: "ian@email.com", phone: "(416) 111-1111", title: "Co-op Student", co_op: true, staff: true, password: "325788" },
   { name: "Emmanuel R", email: "emmanuel@email.com", phone: "(416) 111-1111", title: "Co-op Student", co_op: true, staff: true, password: "619425" },
-  { name: "Nancy V", email: "nancy@email.com", phone: "(416) 111-1111", title: "Co-op Student", co_op: true, staff: true, password: "692890" }])
+  { name: "Nancy V", email: "nancy@email.com", phone: "(416) 111-1111", title: "Co-op Student", co_op: true, staff: true, password: "692890" },
+  { name: "Administrative Account", email: "admin@email.com", phone: "(416) 111-1111", title: "Admin", co_op: true, staff: true, password: "123456" }])
 # Energy
   # { name: "Luxman P.", email: "lup@email.com", phone: "(416) 111-1111", title: "Sr. Business Analyst", business_planning_and_finance: true, staff: true, password: "286670" },
   # { name: "Tai A.", email: "tai@email.com", phone: "(416) 111-1111", title: "Manager (A)", business_planning_and_finance: true, manager: true, password: "493678" },
