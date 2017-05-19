6891a6313c6a077ccf9f7f16ea71322ec9315bbd
dcheley <danielcheley@hotmail.com>
2017-05-19 14:23:23 -0400

    Deleting unused coffee script files, adding signup routes and generating signups controller

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,19 @@
+class SignupsController < ApplicationController
+  def step_one
+  end
+
+  def step_two
+  end
+
+  def step_three
+  end
+
+  def step_four
+  end
+
+  def step_five
+  end
+
+  def step_six
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,20 @@ Rails.application.routes.draw do
 
   get '/pages/*page' => 'pages#show'
 
-  get '/users/org_charts' => 'users#org_charts'
-  get '/users/admo_org_chart' => 'users#admo_org_chart'
-  get '/users/bpf_org_chart' => 'users#bpf_org_chart'
-  get '/users/hr_org_chart' => 'users#hr_org_chart'
-  get '/users/smf_org_chart' => 'users#smf_org_chart'
-  get '/users/opps' => 'users#opps'
-  get '/badges/exp' => 'badges#exp'
+  get '/users/org_charts'
+  get '/users/admo_org_chart'
+  get '/users/bpf_org_chart'
+  get '/users/hr_org_chart'
+  get '/users/smf_org_chart'
+  get '/users/opps'
+  get '/badges/exp'
+
+  get 'signups/step_one'
+  get 'signups/step_two'
+  get 'signups/step_three'
+  get 'signups/step_four'
+  get 'signups/step_five'
+  get 'signups/step_six'
 
   resources :users, only: [:new, :create, :show, :update, :edit, :index]
   resources :opportunities, only: [:show]
