71047c7f37a3d13bc0ae6a524ed7e9fdacfc5429
dcheley <danielcheley@hotmail.com>
2017-05-19 14:34:27 -0400

    Adding strong params including foreign key

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -16,4 +16,10 @@ class SignupsController < ApplicationController
 
   def step_six
   end
+
+private
+
+  def signup_params
+    params.require(:signup).permit(:user_id, :step_one, :step_two, :step_three, :step_four, :step_five, :step_six)
+  end
 end
