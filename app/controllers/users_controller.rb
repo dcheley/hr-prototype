f884ea626ff3633485cc199aa2bb22642db9b9cc
dcheley <danielcheley@hotmail.com>
2017-05-24 15:27:53 -0400

    Adding intranet check box to edit and sign up form fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -94,7 +94,7 @@ private
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :phone,
     :title, :avatar, :job_description, :adm, :director, :manager, :staff, :adm_office,
     :strategic_human_resources, :service_management_and_facilities, :business_planning_and_finance,
-    :co_op, :image_delete, :career_aspirations, :direct_report, :agreement,
+    :co_op, :image_delete, :career_aspirations, :direct_report, :agreement, :intranet,
     signup_attributes: [:user_id, :step_one, :step_two, :step_three, :step_six])
   end
 end
