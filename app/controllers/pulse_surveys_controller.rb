fdc81f9de80df91c5fdbaddd55dbd9a02f5fd756
dcheley <danielcheley@hotmail.com>
2017-05-29 11:58:14 -0400

    Creating strong params, new & create logic and using redirect to logout on survey completion

--- app/controllers/pulse_surveys_controller.rb
+++ app/controllers/pulse_surveys_controller.rb
@@ -1,7 +1,20 @@
 class PulseSurveysController < ApplicationController
   def new
+    @pulse_survey = Pulse_survey.new
   end
 
   def create
+    @pulse_survey = Pulse_survey.new(pulse_survey_params)
+    if @pulse_survey.save
+      session[:user_id] = nil
+      redirect_to root_url, notice: "Survey Complete, Logged out"
+    else
+      render :new
+    end
+  end
+
+private
+  def pulse_survey_params
+    params.require(:pulse_survey).permit(:feedback)
   end
 end
