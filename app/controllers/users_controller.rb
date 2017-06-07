b958701aaf8295a089dae7fa0f8daa3e7020635b
dcheley <danielcheley@hotmail.com>
2017-06-07 11:02:07 -0400

    Calling reset_time in controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
     @opportunity_badges = Badge.where.not(opportunity_id: nil).order("created_at DESC")
     @edu_badges = Badge.where(opportunity_id: nil, education: true).order("created_at DESC")
     @exp_badges = Badge.where(opportunity_id: nil, exp: true).order("created_at DESC")
+    reset_time
   end
 
   def org_charts
