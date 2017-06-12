992041d9fae7cf69fc0443290c083572863b60be
dcheley <danielcheley@hotmail.com>
2017-06-12 17:47:20 -0400

    Adjusting s3 host name variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    s3_host_name: 'http://eeapp.s3.amazonaws.com',
+    s3_host_name: 'eeapp.s3.amazonaws.com',
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
