562c9b713b903a2bab804203f1736a8ab21657bb
dcheley <danielcheley@hotmail.com>
2017-06-22 10:10:11 -0400

    Adding skills string column to opps table

--- db/migrate/20170622140905_add_skillsto_opps.rb
+++ db/migrate/20170622140905_add_skillsto_opps.rb
@@ -1,4 +1,5 @@
 class AddSkillstoOpps < ActiveRecord::Migration[5.0]
   def change
+    add_column :opportunities, :skills, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608161619) do
+ActiveRecord::Schema.define(version: 20170622140905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20170608161619) do
     t.boolean  "learning"
     t.boolean  "career"
     t.boolean  "hobby"
+    t.string   "skills"
   end
 
   create_table "overall_averages", force: :cascade do |t|
