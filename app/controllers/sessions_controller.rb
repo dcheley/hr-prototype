87121d453365fad8f41213ba56ba41c65114348f
dcheley <danielcheley@hotmail.com>
2017-08-10 10:05:12 -0400

    Switching to flash.now to avoid flash remaining on redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to "/users/home"
     else
-      flash[:alert] = "Invalid email or password"
+      flash.now[:alert] = "Invalid email or password"
       render :new
     end
   end
