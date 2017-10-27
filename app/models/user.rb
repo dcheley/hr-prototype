c09e4f595e2e85405288857a10545e6d29436bfa
dcheley <danielcheley@hotmail.com>
2017-10-27 14:54:34 -0400

    Adding custom scopes to team column and comments above each scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,21 @@
 class User < ApplicationRecord
   attr_accessor :password
 
+  # Offices
   scope :adm_office, -> { where(office: 1) }
   scope :business_planning_and_finance, -> { where(office: 2) }
   scope :strategic_human_resources, -> { where(office: 3) }
   scope :service_management_and_facilities, -> { where(office: 4) }
+  # Positions
   scope :director, -> { where(chart_level: 1) }
   scope :manager, -> { where(chart_level: 2) }
   scope :staff, -> { where(chart_level: 3) }
+  # Teams
+  scope :blue, -> { where(team: 1) }
+  scope :red, -> { where(team: 2) }
+  scope :green, -> { where(team: 3) }
+  scope :purple, -> { where(team: 4) }
+  scope :orange, -> { where(team: 5) }
 
   acts_as_voter
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
