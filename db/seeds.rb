3ab865da6309b3a505b52adc2e286f9463a3c9e2
dcheley <danielcheley@hotmail.com>
2017-10-27 15:25:54 -0400

    Unignoring seeds.rb to seed dummy data in production

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+User.create(
+  [ 
+    {name:'Bob', email:'bob@ontario.ca', password:'123456', office: 1, chart_level: 1, team: 1, title:'ADM'},
+    {name:'Sally', email:'sally@ontario.ca', password:'123456', office: 1, chart_level: 2, team: 1, title:'EA'},
+    {name:'Katherine', email:'katherine@ontario.ca', password:'123456', office: 1, chart_level: 3, team: 1, title:'Manager'},
+    {name:'Lee', email:'lee@ontario.ca', password:'123456', office: 1, chart_level: 3, team: 1, title:'Coordinator'},
+    {name:'John', email:'john@ontario.ca', password:'123456', office: 2, chart_level: 1, team: 2, title:'Director'},
+    {name:'Gertika', email:'gertika@ontario.ca', password:'123456', office: 2, chart_level: 2, team: 2, title:'Manager'},
+    {name:'Yuan', email:'yuan@ontario.ca', password:'123456', office: 2, chart_level: 3, team: 2, title:'Associate'},
+    {name:'Jasmine', email:'jasmine@ontario.ca', password:'123456', office: 2, chart_level: 3, team: 2, title:'Analyst'},
+    {name:'Leonard', email:'leonard@ontario.ca', password:'123456', office: 3, chart_level: 1, team: 3, title:'Director'},
+    {name:'Zhen', email:'zhen@ontario.ca', password:'123456', office: 3, chart_level: 2, team: 3, title:'Manager'},
+    {name:'Christina', email:'christina@ontario.ca', password:'123456', office: 3, chart_level: 3, team: 3, title:'Analyst'},
+    {name:'Zdravko', email:'zdravko@ontario.ca', password:'123456', office: 3, chart_level: 3, team: 3, title:'Intern'},
+    {name:'Eduardo', email:'eduardo@ontario.ca', password:'123456', office: 4, chart_level: 1, team: 4, title:'Director'},
+    {name:'Vitam', email:'vitam@ontario.ca', password:'123456', office: 4, chart_level: 2, team: 4, title:'Manager'},
+    {name:'Kael', email:'kael@ontario.ca', password:'123456', office: 4, chart_level: 3, team: 4, title:'Assistant'},
+    {name:'Drake', email:'drake@ontario.ca', password:'123456', office: 4, chart_level: 3, team: 5, title:'Associate'}
+  ]
+)
+
+Opportunity.create(
+  [
+    {name:'OPS Walk & Run Committee', description:'The perfect chance to raise money from your local area for a great cause!', link:'www.fakelink.com', career: true },
+    {name:'AIS Committee', description:'Participate in accomodating your community. We need event managers to help plan and execute large workshops throughout the year.', link:'www.fakelink.com', career: true }
+  ]
+)
