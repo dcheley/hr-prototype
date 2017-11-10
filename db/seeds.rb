bfa6a824401ee2d77da5b51bd37eb08a3d552252
dcheley <danielcheley@hotmail.com>
2017-11-10 15:02:59 -0500

    Updating passwords to satisfy new validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
 User.create(
-  [ 
-    {name:'Bob', email:'bob@ontario.ca', password:'123456', office: 1, chart_level: 1, team: 1, title:'ADM'},
-    {name:'Sally', email:'sally@ontario.ca', password:'123456', office: 1, chart_level: 2, team: 1, title:'EA'},
-    {name:'Katherine', email:'katherine@ontario.ca', password:'123456', office: 1, chart_level: 3, team: 1, title:'Manager'},
-    {name:'Lee', email:'lee@ontario.ca', password:'123456', office: 1, chart_level: 3, team: 1, title:'Coordinator'},
-    {name:'John', email:'john@ontario.ca', password:'123456', office: 2, chart_level: 1, team: 2, title:'Director'},
-    {name:'Gertika', email:'gertika@ontario.ca', password:'123456', office: 2, chart_level: 2, team: 2, title:'Manager'},
-    {name:'Yuan', email:'yuan@ontario.ca', password:'123456', office: 2, chart_level: 3, team: 2, title:'Associate'},
-    {name:'Jasmine', email:'jasmine@ontario.ca', password:'123456', office: 2, chart_level: 3, team: 2, title:'Analyst'},
-    {name:'Leonard', email:'leonard@ontario.ca', password:'123456', office: 3, chart_level: 1, team: 3, title:'Director'},
-    {name:'Zhen', email:'zhen@ontario.ca', password:'123456', office: 3, chart_level: 2, team: 3, title:'Manager'},
-    {name:'Christina', email:'christina@ontario.ca', password:'123456', office: 3, chart_level: 3, team: 3, title:'Analyst'},
-    {name:'Zdravko', email:'zdravko@ontario.ca', password:'123456', office: 3, chart_level: 3, team: 3, title:'Intern'},
-    {name:'Eduardo', email:'eduardo@ontario.ca', password:'123456', office: 4, chart_level: 1, team: 4, title:'Director'},
-    {name:'Vitam', email:'vitam@ontario.ca', password:'123456', office: 4, chart_level: 2, team: 4, title:'Manager'},
-    {name:'Kael', email:'kael@ontario.ca', password:'123456', office: 4, chart_level: 3, team: 4, title:'Assistant'},
-    {name:'Drake', email:'drake@ontario.ca', password:'123456', office: 4, chart_level: 3, team: 5, title:'Associate'}
+  [
+    {name:'Bob', email:'bob@ontario.ca', password:'Letmein#1', office: 1, chart_level: 1, team: 1, title:'ADM'},
+    {name:'Sally', email:'sally@ontario.ca', password:'Letmein#1', office: 1, chart_level: 2, team: 1, title:'EA'},
+    {name:'Katherine', email:'katherine@ontario.ca', password:'Letmein#1', office: 1, chart_level: 3, team: 1, title:'Manager'},
+    {name:'Lee', email:'lee@ontario.ca', password:'Letmein#1', office: 1, chart_level: 3, team: 1, title:'Coordinator'},
+    {name:'John', email:'john@ontario.ca', password:'Letmein#1', office: 2, chart_level: 1, team: 2, title:'Director'},
+    {name:'Gertika', email:'gertika@ontario.ca', password:'Letmein#1', office: 2, chart_level: 2, team: 2, title:'Manager'},
+    {name:'Yuan', email:'yuan@ontario.ca', password:'Letmein#1', office: 2, chart_level: 3, team: 2, title:'Associate'},
+    {name:'Jasmine', email:'jasmine@ontario.ca', password:'Letmein#1', office: 2, chart_level: 3, team: 2, title:'Analyst'},
+    {name:'Leonard', email:'leonard@ontario.ca', password:'Letmein#1', office: 3, chart_level: 1, team: 3, title:'Director'},
+    {name:'Zhen', email:'zhen@ontario.ca', password:'Letmein#1', office: 3, chart_level: 2, team: 3, title:'Manager'},
+    {name:'Christina', email:'christina@ontario.ca', password:'Letmein#1', office: 3, chart_level: 3, team: 3, title:'Analyst'},
+    {name:'Zdravko', email:'zdravko@ontario.ca', password:'Letmein#1', office: 3, chart_level: 3, team: 3, title:'Intern'},
+    {name:'Eduardo', email:'eduardo@ontario.ca', password:'Letmein#1', office: 4, chart_level: 1, team: 4, title:'Director'},
+    {name:'Vitam', email:'vitam@ontario.ca', password:'Letmein#1', office: 4, chart_level: 2, team: 4, title:'Manager'},
+    {name:'Kael', email:'kael@ontario.ca', password:'Letmein#1', office: 4, chart_level: 3, team: 4, title:'Assistant'},
+    {name:'Drake', email:'drake@ontario.ca', password:'Letmein#1', office: 4, chart_level: 3, team: 5, title:'Associate'}
   ]
 )
 
